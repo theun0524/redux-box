fed5e14e3e052f5bd827ca11e306b44d2928180d
theun0524 <theun@postech.ac.kr>
2019-03-30 17:39:42 +0900

    fix for server side rendering

--- src/createStore.js
+++ src/createStore.js
@@ -1,12 +1,13 @@
 import composeEnhancers from "./composeEnhancers";
-import createSagaMiddleware, { delay } from "redux-saga";
+import createSagaMiddleware, { END, delay } from "redux-saga";
 import { all, call } from "redux-saga/effects";
 import getReducer from "./reducer";
+import createSagas from './createSagas';
 import {
-    applyMiddleware,
-    combineReducers,
-    createStore as storeCreator
-  } from "redux";
+  applyMiddleware,
+  combineReducers,
+  createStore as storeCreator
+} from "redux";
 
   
 /*
@@ -17,41 +18,41 @@ we use these arrays to initialize the store using
 */
 export default function createStore(modules, config = {}){
     
-    //Initialize middleware array
-    const sagaMiddleware = createSagaMiddleware(config.sagaMiddlewareOptions);
-    let middlewares = [sagaMiddleware];
-    
-    //push the provided middlewares in config object, to the middleware array
-    if (config && config.middlewares && config.middlewares.length > 0) {
-      middlewares = middlewares.concat(config.middlewares);
-    }
-    let reducerList = Object.assign({}, config.reducers);
-    let sagas = [];
+  //Initialize middleware array
+  const sagaMiddleware = createSagaMiddleware(config.sagaMiddlewareOptions);
+  let middlewares = [sagaMiddleware];
   
-    //iterate through each module and push the sagas and reducers of each module in thier respective array
-    modules.forEach(module => {
-      sagas = sagas.concat(module.sagas);
-      let moduleReducer = getReducer(module.mutations, module.state, module.name);
-      if (module.decorateReducer)
-        moduleReducer = module.decorateReducer(moduleReducer);
-      reducerList[module.name] = moduleReducer;
-    });
-    sagas = config.sagas ? sagas.concat(config.sagas) : sagas;
-    
-    let combinedReducer = combineReducers(reducerList);
-    if (config.decorateReducer) {
-      combinedReducer = config.decorateReducer(combinedReducer);
-    }
-    let preloadedState = config.preloadedState || {};
-    let composeRedux = composeEnhancers(config);
-    //initialize the store using preloaded state, reducers and middlewares
-    let store = storeCreator(
-      combinedReducer,
-      preloadedState,
-      composeRedux(applyMiddleware(...middlewares))
-    );
+  //push the provided middlewares in config object, to the middleware array
+  if (config && config.middlewares && config.middlewares.length > 0) {
+    middlewares = middlewares.concat(config.middlewares);
+  }
+  let reducerList = Object.assign({}, config.reducers);
+  let sagas = [];
 
-// Default configuration for sagas
+  //iterate through each module and push the sagas and reducers of each module in thier respective array
+  modules.forEach(module => {
+    sagas = sagas.concat(createSagas(module.sagas));
+    let moduleReducer = getReducer(module.mutations, module.state, module.name);
+    if (module.decorateReducer)
+      moduleReducer = module.decorateReducer(moduleReducer);
+    reducerList[module.name] = moduleReducer;
+  });
+  sagas = config.sagas ? sagas.concat(config.sagas) : sagas;
+  
+  let combinedReducer = combineReducers(reducerList);
+  if (config.decorateReducer) {
+    combinedReducer = config.decorateReducer(combinedReducer);
+  }
+  let preloadedState = config.preloadedState || {};
+  let composeRedux = composeEnhancers(config);
+  //initialize the store using preloaded state, reducers and middlewares
+  let store = storeCreator(
+    combinedReducer,
+    preloadedState,
+    composeRedux(applyMiddleware(...middlewares))
+  );
+
+  // Default configuration for sagas
   const sagaConfig = Object.assign(
     {},
     {
@@ -61,16 +62,19 @@ export default function createStore(modules, config = {}){
     config.sagaConfig
   );
    
-    function* rootSaga() {
-      while (true) {
-        try {
-          yield all(sagas);
-        } catch (err) {
-          sagaConfig.onError(err);
-          yield call(delay, sagaConfig.retryDelay);
-        }
+  function* rootSaga() {
+    while (true) {
+      try {
+        yield all(sagas);
+      } catch (err) {
+        sagaConfig.onError(err);
+        yield call(delay, sagaConfig.retryDelay);
       }
     }
-    sagaMiddleware.run(rootSaga);
-    return store;
-  };
+  }
+
+  store.close = () => store.dispatch(END);
+  store.runSaga = sagaMiddleware.run(rootSaga);
+  
+  return store;
+};
